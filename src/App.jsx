756ac831a1fbb0ecf8da5a888f8e7f820e3a8871
user_11_20P1 <20p1_11@mail.ru>
2024-11-15 11:45:41 +0500

    tabs component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Tab, Tabs } from "./components/Tabs";
+
 export const App = () =>{
   return (
     <div className="container">
-      <button className="btn btn_black">Test</button>
-      <br />
-      <button className="btn btn_red">Test</button>
+      <Tabs>
+        <Tab label="tab 1">
+          <h1>tab1</h1>
+        </Tab>
+        <Tab label="tab 2">
+          <h1>tab2</h1>
+        </Tab>
+        <Tab label="tab 3">
+          <h1>tab3</h1>
+        </Tab>
+      </Tabs>
 
       
     </div>
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,14 +1,57 @@
-import { Children } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 
-export const Tabs = ({children})=>{
+export const Tabs = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const [changing, setChanging] = useState(false);
+  const [underlineWidth, setUnderlineWidth] = useState(0);
+  const [underlineLeft, setUnderlineLeft] = useState(0);
+  const tabRefs = useRef([]);
 
-    return (
-        <div className="tab tab_menu">
-            {Children.map(children, child=>{
-                if (child.props.className.includes('tab_header')){
-                    return 
-                }
-            })}
-        </div>
-    );
+  useEffect(() => {
+    const index = children.findIndex((child) => child.props.label === activeTab);
+    const { width, left } = tabRefs.current[index].getBoundingClientRect();
+    setUnderlineWidth(width);
+    setUnderlineLeft(left);
+  }, [activeTab, children]);
+
+  const handleClick = (tab) => {
+    setChanging(true);
+    setTimeout(()=>{
+      setActiveTab(tab);
+      setChanging(false);
+    }, 500);
+    
+
+  }
+
+  return (
+    <div className="tab">
+      <div className="tab_list">
+        {children.map((child, index) => (
+          <h4
+            key={child.props.label}
+            ref={(el) => (tabRefs.current[index] = el)}
+            className={child.props.label === activeTab ? 'actve' : ''}
+            onClick={() => handleClick(child.props.label)}
+          >
+            {child.props.label}
+          </h4>
+        ))}
+        <div
+          className="underline"
+          style={{ width: underlineWidth, left: underlineLeft - 10 }}
+        />
+      </div>
+      <hr />
+      <div className="tab_content">
+        {children.map((child) => (
+          child.props.label === activeTab && <div key={child.props.label} className={`animated ${changing ? 'disable' : ''}`}>{child.props.children}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export const Tab = ({ label, children }) => {
+  return <>{children}</>;
 }
